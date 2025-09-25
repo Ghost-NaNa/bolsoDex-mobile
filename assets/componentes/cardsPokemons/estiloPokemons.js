70addd590715dcb343f654917d2db1fef10d664a
Nathan <nathan.oltavares@gmail.com>
2025-09-24 22:14:39 -0300

    pequena atualização
    agora os tipos vem com uma cor representando cada

--- assets/componentes/cardsPokemons/estiloPokemons.js
+++ assets/componentes/cardsPokemons/estiloPokemons.js
@@ -29,14 +29,32 @@ const styles = StyleSheet.create({
 		textTransform: 'capitalize',
 	},
 	tipo: {
+		width: '100%',
 		fontSize: 12,
-		color: '#666',
-		margin: 2,
 		paddingHorizontal: 6,
 		paddingVertical: 2,
 		borderRadius: 6,
-		backgroundColor: '#e5e5e5',
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 6
 	}
 });
 
-export default styles;
+const typeStyles = StyleSheet.create({
+	fire: { backgroundColor: '#F08030', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	water: { backgroundColor: '#6890F0', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	grass: { backgroundColor: '#78C870', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	poison: { backgroundColor: '#A040A0', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	electric: { backgroundColor: '#F8D030', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	ground: { backgroundColor: '#E0C068', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	ice: { backgroundColor: '#98D8D8', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	fighting: { backgroundColor: '#C03028', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	psychic: { backgroundColor: '#F85888', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	flying: { backgroundColor: '#A890F0', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	bug: { backgroundColor: '#A8B820', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	rock: { backgroundColor: '#B8A038', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	normal: { backgroundColor: '#A8A878', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+	fairy: { backgroundColor: '#EE99AC', color: '#fff', width: 70, textAlign: 'center', borderRadius: 6, padding: 2, textTransform: 'capitalize'  },
+})
+
+export { styles, typeStyles};
--- assets/componentes/cardsPokemons/pokemon.js
+++ assets/componentes/cardsPokemons/pokemon.js
@@ -1,7 +1,7 @@
 import { View, Text, Image, TouchableHighlight, TouchableOpacity } from "react-native";
-import styles from "./estiloPokemons";
+import { styles, typeStyles } from "./estiloPokemons.js";
 
-export default function PokemonCard({ pokemonId, pokemonName, sprite }) {
+export default function PokemonCard({ pokemonId, pokemonName, sprite, pokemonTipo1, pokemonTipo2 }) {
     return (
         <TouchableOpacity style={styles.card}>
             <View>
@@ -13,13 +13,50 @@ export default function PokemonCard({ pokemonId, pokemonName, sprite }) {
 
                 <View style={styles.tipo}>
                     <View>
-                        <Text>Tipo</Text>
+                        <Text style={verificarTipo(pokemonTipo1)}>{pokemonTipo1}</Text>
                     </View>
                     <View>
-                        <Text>Tipo</Text>
+                        <Text style={verificarTipo(pokemonTipo2)}>{pokemonTipo2}</Text>
                     </View>
                 </View>
             </View>
         </TouchableOpacity>
     )
+}
+
+function verificarTipo(tipo) {
+    switch (tipo) {
+        case 'fire':
+            return typeStyles.fire;
+        case 'water':
+            return typeStyles.water;
+        case 'grass':
+            return typeStyles.grass;
+        case 'poison':
+            return typeStyles.poison;
+        case 'electric':
+            return typeStyles.electric;
+        case 'ground':
+            return typeStyles.ground;
+        case 'ice':
+            return typeStyles.ice;
+        case 'fighting':
+            return typeStyles.fighting;
+        case 'psychic':
+            return typeStyles.psychic;
+        case 'flying':
+            return typeStyles.flying;
+        case 'bug':
+            return typeStyles.bug;
+        case 'rock':
+            return typeStyles.rock;
+        case 'poison':
+            return typeStyles.poison;
+        case 'normal':
+            return typeStyles.normal;
+        case 'fairy':
+            return typeStyles.fairy;
+        default:
+            return {};
+    }
 }
